5f8b9152aed65b98d61f431cf5644b28dc537c79
NoelCanMen <99173650+NoelCanMen@users.noreply.github.com>
2022-02-07 10:10:15 +0100

    Add files via upload

---
+++ holamundo.java
@@ -0,0 +1,25 @@
+
+
+public class holamundo 
+{
+	final static float IVA=(float) 0.18;//float para convertir a float obligatoriamente
+	final static char CTE1='*';
+	final static int CTE2=300;
+	final static int CTE=0;
+	final static boolean SI=true;
+	final static boolean NO=false;
+	
+	public static void main(String[] args) 
+	{
+		int a;
+		a=CTE2;
+		System.out.println("Hola");
+		System.out.println("\t mundo");
+		System.out.println("\t \t adios");
+		System.out.println(IVA);
+		System.out.println(a);
+		
+
+	}
+
+}
